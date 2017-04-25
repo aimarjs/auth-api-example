192a67413a61f38c26e8fec642237dd2a7ecd323
Aimar Aareleid <aimar@snapit.ee>
2017-04-25 17:42:54 +0200

    update

--- app/controllers/Auth.js
+++ app/controllers/Auth.js
@@ -1,5 +1,6 @@
 exports.signup = function(req, res, next) {
   res.status(200).send({ message: 'Go ahead, start siging up!'})
+  console.log(req.body);
 }
 
 exports.login = function(req, res, next) {
